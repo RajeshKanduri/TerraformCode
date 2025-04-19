3c3fca0d7ed63520043ff18de785a1098f7ae001
Rajesh Kanduri <Rajesh@Rajesh-Mac-Book.local>
2025-04-19 15:17:56 +0530

    Day-1

---
+++ Day-1-Basic-Code/variable.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    description = "inserting amiid into main"
+    type = string
+    default = "ami-0e449927258d45bc4"
+}
+    variable "instance_type" { 
+        type = string
+        default = "t2.nano"
+    }
+    #onlys ingle value is calling that is string and multiple values callig we can use like a list
+  
